28b822ea52184ac150bd859e27a3cb2302095fbb
Joseph Clement <jocl6895@colorado.edu>
2023-10-04 14:09:53 -0400

    removed unused files

--- firstcondor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# file name: firstcondor.sh
-
-source /opt/sphenix/core/bin/sphenix_setup.sh -n
-source /opt/sphenix/core/bin/setup_local.sh "/sphenix/user/jocl/projects/testinstall"
-export TESTINSTALL="/sphenix/user/jocl/projects/testinstall"
-export HOME=/sphenix/u/jocl
-root -b -q 'run_macro.C('${1}')'
